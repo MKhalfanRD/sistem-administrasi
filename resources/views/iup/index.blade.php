da6b30aa286ad08b46d355a7a1de8bb8b8a68e46
MKhalfanRD <khalfanrd2002@gmail.com>
2024-03-11 00:05:10 +0700

    table expand udah muncul namun masih berantakan

--- resources/views/iup/index.blade.php
+++ resources/views/iup/index.blade.php
@@ -55,125 +55,150 @@
                         $counter = 0;
                     @endphp
                     @foreach ($IUP as $iup)
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <div class="row-content">
-                            <th class="px-6 py-4">
-                                @php
-                                    $counter++;
-                                @endphp
-                                {{$counter}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->namaPerusahaan}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->alamat}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->npwp}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->nib}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->kabupaten}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->luasWilayah}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->komoditas}}
-                            </th>
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{$iup->lokasiIzin}}
-                            </th>
-                            <th scope="row" class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                @if ($iup->statusIzin == 'Aktif')
-                                    <span class="bg-green-300 px-3 py-2 text-green-700 rounded-md">Aktif</span>
-                                @else
-                                    <span class="bg-red-300 px-2 py-2 text-red-700 rounded-md">Tidak Aktif</span>
-                                @endif
-                            </th>
-                            <th scope="row" class="text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                @if ($iup->scanSK)
-                                    <a href="{{asset('storage/' .$iup->scanSK)}}" target="_blank">
-                                        <img src="{{ asset('icon/ikon-pdf.png') }}" class="bg-gray-300 hover:bg-gray-200 rounded-md p-1 w-10 h-10 mx-auto" alt="PDF Icon">
-                                    </a>
-                                @else
-                                    <p>Tidak Ada</p>
-                                @endif
-                            </th>
-                        </div>
-                        <div class="details-content hidden">
-                            <table class="table-auto w-full">
-                                <thead>
-                                  <tr>
-                                    <th class="px-4 py-2 text-center">
-                                      WIUP
-                                      <div class="flex flex-row space-x-2">
-                                        <span class="text-sm">Tanggal SK</span>
-                                        <span class="text-sm">No SK</span>
-                                      </div>
-                                    </th>
-                                    <th class="px-4 py-2 text-center">
-                                      IUP Eksplorasi
-                                      <div class="flex flex-row space-x-2">
-                                        <span class="text-sm">Tanggal SK</span>
-                                        <span class="text-sm">No SK</span>
-                                        <span class="text-sm">Masa Berlaku</span>
-                                        <span class="text-sm">Tanggal Berakhir</span>
-                                      </div>
-                                    </th>
-                                    <th class="px-4 py-2 text-center">
-                                      IUP Operasi Produksi
-                                      <div class="flex flex-row space-x-2">
-                                        <span class="text-sm">Tanggal SK</span>
-                                        <span class="text-sm">No SK</span>
-                                        <span class="text-sm">Masa Berlaku</span>
-                                        <span class="text-sm">Tanggal Berakhir</span>
-                                      </div>
-                                    </th>
-                                    <th class="px-4 py-2 text-center">
-                                      Perpanjangan 1
-                                      <div class="flex flex-row space-x-2">
-                                        <span class="text-sm">Tanggal SK</span>
-                                        <span class="text-sm">No SK</span>
-                                        <span class="text-sm">Masa Berlaku</span>
-                                        <span class="text-sm">Tanggal Berakhir</span>
-                                      </div>
-                                    </th>
-                                    <th class="px-4 py-2 text-center">
-                                      Perpanjangan 2
-                                      <div class="flex flex-row space-x-2">
-                                        <span class="text-sm">Tanggal SK</span>
-                                        <span class="text-sm">No SK</span>
-                                        <span class="text-sm">Masa Berlaku</span>
-                                        <span class="text-sm">Tanggal Berakhir</span>
-                                      </div>
-                                    </th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td class="px-4 py-2 border"></td>
-                                    <td class="px-4 py-2 border"></td>
-                                  </tr>
-                                </tbody>
-                              </table>
-
-                        </div>
-                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            <div class="btn-wrap flex flex-row gap-3">
-                                <a href="{{route('iup.edit', $iup->id)}}">
-                                    <button type="submit" class="bg-indigo-600 hover:bg-indigo-500 text-white px-5 py-2.5 rounded-lg">Edit</button>
-                                </a>
-                                <form action="{{route('iup.destroy', $iup->id)}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="text-white hover:bg-gray-700 px-5 py-2.5 rounded-lg border border-gray-300">Delete</button>
-                                </form>
-                            </div>
+                    <tr class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600 expandable-row cursor-pointer">
+                        <th class="px-6 py-4">
+                            @php
+                                $counter++;
+                            @endphp
+                            {{$counter}}
                         </th>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->namaPerusahaan}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->alamat}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->npwp}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->nib}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->kabupaten}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->luasWilayah}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->komoditas}}
+                        </td>
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{$iup->lokasiIzin}}
+                        </td>
+                        <td class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            @if ($iup->statusIzin == 'Aktif')
+                                <span class="bg-green-300 px-3 py-2 text-green-700 rounded-md">Aktif</span>
+                            @else
+                                <span class="bg-red-300 px-2 py-2 text-red-700 rounded-md">Tidak Aktif</span>
+                            @endif
+                        </td>
+                        <td class="text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            @if ($iup->scanSK)
+                                <a href="{{asset('storage/' .$iup->scanSK)}}" target="_blank">
+                                    <img src="{{ asset('icon/ikon-pdf.png') }}" class="bg-gray-300 hover:bg-gray-200 rounded-md p-1 w-10 h-10 mx-auto" alt="PDF Icon">
+                                </a>
+                            @else
+                                <p>Tidak Ada</p>
+                            @endif
+                        </td>
+                    </tr>
+                    <tr class="expandable-content hidden">
+                        <td colspan="12">
+                            <div class="details-content">
+                                <table class="table-auto w-full">
+                                    <thead class="text-base text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                        <tr>
+                                            <th class="px-4 py-2 text-center flex flex-col">
+                                                WIUP
+                                                <div class="gap-5">
+                                                    <th class="text-sm">Tanggal SK</th>
+                                                    <th class="text-sm">No SK</th>
+                                                </div>
+                                            </th>
+                                            <th class="px-4 py-2 text-center flex flex-col">
+                                                IUP Eksplorasi
+                                                <div class="flex flex-row gap-5">
+                                                    <th class="text-sm">Tanggal SK</th>
+                                                    <th class="text-sm">No SK</th>
+                                                    <th class="text-sm">Masa Berlaku</th>
+                                                    <th class="text-sm">Tanggal Berakhir</th>
+                                                </div>
+                                            </th>
+                                            <th class="px-4 py-2 text-center">
+                                                IUP Operasi Produksi
+                                                <div class="flex flex-row gap-5">
+                                                    <th class="text-sm">Tanggal SK</th>
+                                                    <th class="text-sm">No SK</th>
+                                                    <th class="text-sm">Masa Berlaku</th>
+                                                    <th class="text-sm">Tanggal Berakhir</th>
+                                                </div>
+                                            </th>
+                                            <th class="px-4 py-2 text-center">
+                                                Perpanjangan 1
+                                                <div class="flex flex-row gap-5">
+                                                    <th class="text-sm">Tanggal SK</th>
+                                                    <th class="text-sm">No SK</th>
+                                                    <th class="text-sm">Masa Berlaku</th>
+                                                    <th class="text-sm">Tanggal Berakhir</th>
+                                                </div>
+                                            </th>
+                                            <th class="px-4 py-2 text-center">
+                                                Perpanjangan 2
+                                                <div class="flex flex-row gap-5">
+                                                    <th class="text-sm">Tanggal SK</th>
+                                                    <th class="text-sm">No SK</th>
+                                                    <th class="text-sm">Masa Berlaku</th>
+                                                    <th class="text-sm">Tanggal Berakhir</th>
+                                                </div>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="bg-white dark:bg-gray-800  hover:bg-gray-50 dark:hover:bg-gray-600">
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <div class="flex flex-row text-sm">
+                                                    <td>{{$iup->tanggalSK}}</td>
+                                                    <td>{{$iup->noSK}}</td>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white flex flex-row gap-20">
+                                                <div class="space-x-5 text-center">
+                                                    <td>{{$iup->tanggalSK}}</td>
+                                                    <td>{{$iup->noSK}}</td>
+                                                    <td>{{$iup->masaBerlaku}}</td>
+                                                    <td>{{$iup->tanggalBerakhir}}</td>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <div class="flex flex-row gap-5 text-sm">
+                                                    <td>{{$iup->tanggalSK}}</td>
+                                                    <td>{{$iup->noSK}}</td>
+                                                    <td>{{$iup->masaBerlaku}}</td>
+                                                    <td>{{$iup->tanggalBerakhir}}</td>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <div class="flex flex-row gap-5 text-sm">
+                                                    <td>{{$iup->tanggalSK}}</td>
+                                                    <td>{{$iup->noSK}}</td>
+                                                    <td>{{$iup->masaBerlaku}}</td>
+                                                    <td>{{$iup->tanggalBerakhir}}</td>
+                                                </div>
+                                            </td>
+                                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                <div class="flex flex-row gap-5 text-sm">
+                                                    <td>{{$iup->tanggalSK}}</td>
+                                                    <td>{{$iup->noSK}}</td>
+                                                    <td>{{$iup->masaBerlaku}}</td>
+                                                    <td>{{$iup->tanggalBerakhir}}</td>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
@@ -182,13 +207,18 @@
     </div>
     <script>
         document.addEventListener('DOMContentLoaded', function(){
-            const rows = document.querSelectorAll('tr');
+            const rows = document.querySelectorAll('tr.expandable-row');
             rows.forEach(row => {
                 row.addEventListener('click', function(){
-                    const detailsContent = this.querySelector('.details-content');
-                    detailsContent.classList.toggle('hidden');
-                })
-            })
-        })
+                    console.log('Row clicked:', this);
+                    const detailsContent = this.nextElementSibling;
+                    if(detailsContent && detailsContent.classList.contains('expandable-content')){
+                        detailsContent.classList.toggle('hidden');
+                    }else{
+                        console.error('No expandable-content found after expandable-row.')
+                    }
+                });
+            });
+        });
     </script>
 @endsection
